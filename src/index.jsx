3ed8e47fb723f3375b4350626e903f502ba5d0ef
unknown <jusleytavares@ioasys.com.br>
2021-07-30 21:16:55 -0300

    feat: add IntlProvider

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import IntlProviderWrapper from './providers/IntlProviderWrapper';
+import theme from './styles/theme';
+import Routes from './routes.jsx';
+import { store } from './store';
+import GlobalStyles from './styles/global';
+import './assets/fonts/Montserrat/style.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <IntlProviderWrapper>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Routes />
+      </ThemeProvider>
+    </IntlProviderWrapper>
+  </Provider>,
   document.getElementById('root'),
 );
-
-reportWebVitals();
---
+++ src/lib/core/language.jsx
@@ -0,0 +1,13 @@
+export const flattenMessages = (nestedMessages, prefix = '') =>
+  Objext.keys(nestedMessages).reduce((messages, key) => {
+    const value = nestedMessages[key];
+
+    const prefixedKey = prefix ? `${prefix}.${key}` : key;
+
+    const flattenMessage =
+      typeof value === 'string'
+        ? { [prefixedKey]: value }
+        : flattenMessages(value, prefixedKey);
+
+    return Object.assign(messages, flattenMessages);
+  }, {});
